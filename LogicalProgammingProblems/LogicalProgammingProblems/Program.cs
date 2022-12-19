7bbc58a77bdafb908355e77a63b91558a010c32c
qismat <qismatnisha6243@gmail.com>
2022-12-19 08:58:10 +0530

    [qismat] Add : UC2-PerfectNumber

--- LogicalProgammingProblems/LogicalProgammingProblems/Program.cs
+++ LogicalProgammingProblems/LogicalProgammingProblems/Program.cs
@@ -4,8 +4,22 @@
     {
         static void Main(string[] args)
         {
-            FibonacciSeries fibonacciSeries = new FibonacciSeries();
-            fibonacciSeries.FibonoccSeriesNumber();
+            while (true)
+            {
+                Console.WriteLine("Select program \n1.Fibonacci series\n2 Given Number is Perfect or Not");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.FibonoccSeriesNumber();
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.PerfectNum();
+                        break;
+                }
+            }
         }
     }
 }
